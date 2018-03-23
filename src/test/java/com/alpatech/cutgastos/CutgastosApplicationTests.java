2d9c8ebd662c5b72f651035f3f6b8264b2f50a83
Pedro Duarte <pedrolu.duarte@gmail.com>
2018-03-23 20:03:02 -0300

    First Commit

---
+++ src/test/java/com/alpatech/cutgastos/CutgastosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alpatech.cutgastos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CutgastosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
