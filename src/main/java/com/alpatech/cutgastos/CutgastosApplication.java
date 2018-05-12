367dd6e127cb39908a60bfbc9f7aff632747715b
Pedro Duarte <pedrolu.duarte@gmail.com>
2018-05-12 16:22:39 -0300

    Inclusão get raiz

--- src/main/java/com/alpatech/cutgastos/CutgastosApplication.java
+++ src/main/java/com/alpatech/cutgastos/CutgastosApplication.java
@@ -3,17 +3,17 @@ package com.alpatech.cutgastos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class CutgastosApplication /*extends SpringBootServletInitializer*/{
 
-	/*@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
-		return applicationBuilder.sources(CutgastosApplication.class);
-	}*/
-
+	@RequestMapping("/")
+	String home(){
+		return "Bem vindo ao cutgastos!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CutgastosApplication.class, args);
 	}
