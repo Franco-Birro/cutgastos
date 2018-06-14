10c72ffc9e8b5ebcbc567d84f0521d1cf633a999
Pedro Duarte <pedrolu.duarte@gmail.com>
2018-06-13 23:47:37 -0300

    Cross Origin

--- src/main/java/com/alpatech/cutgastos/CutgastosApplication.java
+++ src/main/java/com/alpatech/cutgastos/CutgastosApplication.java
@@ -3,6 +3,7 @@ package com.alpatech.cutgastos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CutgastosApplication /*extends SpringBootServletInitializer*/{
 
+	@CrossOrigin(origins = "*")
 	@RequestMapping("/")
 	String home(){
 		return "Bem vindo ao cutgastos!";
